cacc7ce0c8257d27107fd030cb5e7063a79d561d
Bistik <kungperl@gmail.com>
2014-03-03 23:50:38 +0800

    throw exception for unwanted attributes

--- lib/Player.pm
+++ lib/Player.pm
@@ -18,14 +18,19 @@ sub _initialize {
 
 
     foreach my $attr ( qw/last_name first_name/ ) {
-        print "attr: $attr\n";
         unless ( $arg_for{$attr} ) {
             croak "$attr cannot be blank";
         }
+        $self->{$attr} = delete $arg_for{$attr};
     }
-    my $jersey = $arg_for{jersey};
+    my $jersey = delete $arg_for{jersey} if $arg_for{jersey};
     unless ( $jersey && $jersey =~ /\d/ ) {
         croak "jersey cannot be blank and must be a number";
+    }
+    $self->{jersey} = $jersey;
+
+    foreach my $extra_attr ( keys %arg_for ) {
+        croak "Invalid attribute '$extra_attr'";
     }
 }
 
